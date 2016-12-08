239151e2fed48c55c075f74fc1ebedc201e0519e
liyong.chu <liyong.chu@weimob.com>
2016-12-08 17:07:17 +0800

    1111

--- src/main/java/cn/tomoya/util/IpUtil.java
+++ src/main/java/cn/tomoya/util/IpUtil.java
@@ -7,11 +7,13 @@ public class IpUtil {
     }
 
     public static String getIpAddr(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
+        String ip = request.getHeader("X-real-ip");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("x-forwarded-for");
+        }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
-
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
