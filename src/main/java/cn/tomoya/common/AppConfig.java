805fb42de2107d3060cdd54d2b869f3f17d3eb7d
朋也 <tomoya92@users.noreply.github.com>
2016-12-16 10:37:35 +0800

    update port

--- src/main/java/cn/tomoya/common/AppConfig.java
+++ src/main/java/cn/tomoya/common/AppConfig.java
@@ -104,6 +104,6 @@ public class AppConfig extends JFinalConfig {
      * 运行此 main 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
      */
     public static void main(String[] args) {
-        JFinal.start("src/main/webapp", 4000, "/", 5);
+        JFinal.start("src/main/webapp", 8080, "/", 5);
     }
 }
