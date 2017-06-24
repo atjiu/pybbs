17fb962682c4825ce6c2608053109b9f1ef9a51f
朋也 <py2qiuse@gmail.com>
2017-06-24 17:16:42 +0800

    更新初始化sql

--- pybbs.sql
+++ pybbs.sql
@@ -1,85 +1,42 @@
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (1,'仪表盘','index',0,NULL);
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (2,'话题','topic',0,NULL);
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (3,'回复','reply',0,NULL);
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (4,'用户','user',0,NULL);
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (5,'权限','security',0,NULL);
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (6,'角色','role',0,NULL);
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (7,'后台管理','admin:index',1,'/admin/index');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (8,'话题编辑（慎勾！不是自己的话题也可以编辑）','topic:edit',2,'/topic/*/edit');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (9,'话题删除（慎勾！不是自己的话题也可以删除）','topic:delete',2,'/topic/*/delete');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (10,'回复编辑（慎勾！不是自己的回复也可以编辑）','reply:edit',3,'/reply/*/edit');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (11,'回复删除（慎勾！不是自己的回复也可以删除）','reply:delete',3,'/reply/*/delete');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (12,'回复列表','reply:list',3,'/admin/reply/list');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (13,'用户列表','user:list',4,'/admin/user/list');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (14,'用户角色','user:role',4,'/admin/user/*/role');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (15,'用户删除','user:delete',4,'/admin/user/*/delete');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (16,'权限列表','permission:list',5,'/admin/permission/list');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (17,'权限添加','permission:add',5,'/admin/permission/add');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (18,'权限编辑','permission:edit',5,'/admin/permission/*/edit');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (19,'权限删除','permission:delete',5,'/admin/permission/*/delete');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (20,'角色列表','role:list',6,'/admin/role/list');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (21,'角色添加','role:add',6,'/admin/role/add');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (22,'角色编辑','role:edit',6,'/admin/role/*/edit');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (23,'角色删除','role:delete',6,'/admin/role/*/delete');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (24,'索引','index',0,'');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (25,'索引全部话题','index:all',24,'/admin/indexed/indexAll');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (26,'删除所有索引','index:deleteAll',24,'/admin/indexed/deleteAll');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (27,'话题置顶','topic:top',2,'/topic/*/top');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (28,'话题加精','topic:good',2,'/topic/*/good');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (29,'版块','section',0,NULL);
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (30,'版块列表','section:list',29,'/admin/section/list');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (31,'添加版块','section:add',29,'/admin/section/add');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (32,'编辑版块','section:edit',29,'/admin/section/*/edit');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (33,'删除版块','section:delete',29,'/admin/section/*/delete');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (34,'标签','label',0,'');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (35,'标签列表','label:list',34,'/admin/label/list');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (36,'标签编辑','label:edit',34,'/admin/label/*/edit');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (37,'标签更新','label:update',34,'/admin/label/update');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (38,'标签删除','label:delete',34,'/admin/label/*/delete');
-INSERT INTO `pybbs_permission` (`id`,`description`,`name`,`pid`,`url`) VALUES (39,'后台话题列表','topic:list',2,'/admin/topic/list');
+INSERT INTO `pybbs_permission`
+VALUES (1, '仪表盘', 'index', 0, NULL), (2, '话题', 'topic', 0, NULL), (3, '回复', 'reply', 0, NULL),
+  (4, '用户', 'user', 0, NULL), (5, '权限', 'security', 0, NULL), (6, '角色', 'role', 0, NULL),
+  (7, '后台管理', 'admin:index', 1, '/admin/index'), (8, '话题编辑', 'topic:edit', 2, '/admin/topic/*/edit'),
+  (9, '话题删除', 'topic:delete', 2, '/admin/topic/*/delete'), (10, '回复编辑', 'reply:edit', 3, '/admin/reply/*/edit'),
+  (11, '回复删除', 'reply:delete', 3, '/admin/reply/*/delete'), (12, '回复列表', 'reply:list', 3, '/admin/reply/list'),
+  (13, '用户列表', 'user:list', 4, '/admin/user/list'), (14, '用户角色', 'user:role', 4, '/admin/user/*/role'),
+  (15, '用户删除', 'user:delete', 4, '/admin/user/*/delete'), (16, '权限列表', 'permission:list', 5, '/admin/permission/list'),
+  (17, '权限添加', 'permission:add', 5, '/admin/permission/add'),
+  (18, '权限编辑', 'permission:edit', 5, '/admin/permission/*/edit'),
+  (19, '权限删除', 'permission:delete', 5, '/admin/permission/*/delete'), (20, '角色列表', 'role:list', 6, '/admin/role/list'),
+  (21, '角色添加', 'role:add', 6, '/admin/role/add'), (22, '角色编辑', 'role:edit', 6, '/admin/role/*/edit'),
+  (23, '角色删除', 'role:delete', 6, '/admin/role/*/delete'), (24, '索引', 'index', 0, ''),
+  (25, '索引全部话题', 'index:all', 24, '/admin/indexed/indexAll'),
+  (26, '删除所有索引', 'index:deleteAll', 24, '/admin/indexed/deleteAll'), (27, '话题置顶', 'topic:top', 2, '/admin/topic/*/top'),
+  (28, '话题加精', 'topic:good', 2, '/admin/topic/*/good'), (29, '版块', 'section', 0, NULL),
+  (30, '版块列表', 'section:list', 29, '/admin/section/list'), (31, '添加版块', 'section:add', 29, '/admin/section/add'),
+  (32, '编辑版块', 'section:edit', 29, '/admin/section/*/edit'),
+  (33, '删除版块', 'section:delete', 29, '/admin/section/*/delete'), (34, '标签', 'label', 0, ''),
+  (35, '标签列表', 'label:list', 34, '/admin/label/list'), (36, '标签编辑', 'label:edit', 34, '/admin/label/*/edit'),
+  (37, '标签更新', 'label:update', 34, '/admin/label/update'), (38, '标签删除', 'label:delete', 34, '/admin/label/*/delete'),
+  (39, '后台话题列表', 'topic:list', 2, '/admin/topic/list'), (40, '话题锁定', 'topic:lock', 2, '/admin/topic/*/lock');
 
 INSERT INTO `pybbs_role` VALUES (1, '超级管理员', 'admin'), (2, '版主', 'banzhu'), (3, '会员', 'user');
 
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,7);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (2,7);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,8);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (2,8);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,9);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (2,9);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,10);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (2,10);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,11);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (2,11);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,12);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (2,12);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,13);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,14);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,15);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,16);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,17);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,18);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,19);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,20);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,21);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,22);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,23);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,25);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,26);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,27);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,28);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,30);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,31);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,32);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,33);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,35);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,36);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,37);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,38);
-INSERT INTO `pybbs_role_permission` (`role_id`,`permission_id`) VALUES (1,39);
+INSERT INTO `pybbs_role_permission`
+VALUES (1, 7), (2, 7), (1, 8), (2, 8), (1, 9), (2, 9), (1, 10), (2, 10), (1, 11), (2, 11), (1, 12), (2, 12), (1, 13),
+  (1, 14), (1, 15), (1, 16), (1, 17), (1, 18), (1, 19), (1, 20), (1, 21), (1, 22), (1, 23), (1, 25), (1, 26), (1, 27),
+  (2, 27), (1, 28), (2, 28), (1, 30), (1, 31), (1, 32), (1, 33), (1, 35), (1, 36), (1, 37), (1, 38), (1, 39), (2, 39),
+  (1, 40), (2, 40);
 
 INSERT INTO `pybbs_section` VALUES (1, '分享'), (4, '博客'), (5, '招聘'), (3, '新闻'), (2, '问答');
 
-INSERT INTO `pybbs_user` (`id`,`avatar`,`block`,`email`,`in_time`,`password`,`signature`,`token`,`url`,`username`,`attempts`,`attempts_time`,`score`,`space_size`) VALUES (1,'http://localhost:8080/static/images/upload/avatar/default.png',false,'liygheart@qq.com','2016-09-09 09:50:14','$2a$10$KkUG107R3ASTHfAHei.bweXWXgCa4cE1KhK.F0odzfE0r97aeeTXC','hello world','d20b9a5c-8693-41a6-8943-ddb2cb78eebd','https://tomoya92.github.io','tomoya',0,'2017-06-21 23:11:24',2000,50);
+INSERT INTO `pybbs_user` (`id`, `avatar`, `block`, `email`, `in_time`, `password`, `signature`, `token`, `url`, `username`, `attempts`, `attempts_time`, `score`, `space_size`)
+VALUES
+  (1, 'http://localhost:8080/static/images/upload/avatar/default.png', FALSE, 'liygheart@qq.com', '2016-09-09 09:50:14',
+      '$2a$10$KkUG107R3ASTHfAHei.bweXWXgCa4cE1KhK.F0odzfE0r97aeeTXC', 'hello world',
+      'd20b9a5c-8693-41a6-8943-ddb2cb78eebd', 'https://tomoya92.github.io', 'tomoya', 0, '2017-06-21 23:11:24', 2000,
+   50);
 
 INSERT INTO `pybbs_user_role` VALUES (1, 1);
 
