b8d81843b6fb5e110e1c8d5fd1e5a47116448f1b
tomoya <py2qiuse@gmail.com>
2017-10-25 10:08:22 +0800

    修复登录后跳转不到自定义链接的问题

--- src/main/java/co/yiiu/config/SecurityConfig.java
+++ src/main/java/co/yiiu/config/SecurityConfig.java
@@ -13,11 +13,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by tomoya.
@@ -69,13 +77,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authenticated();
 
     http.formLogin()
-        .successHandler(savedRequestAwareAuthenticationSuccessHandler())
-        .loginPage(siteConfig.getBaseUrl() + "/login")
-        .loginProcessingUrl(siteConfig.getBaseUrl() + "/login")
-        .usernameParameter("username")
-        .passwordParameter("password")
-        .failureUrl(siteConfig.getBaseUrl() + "/login?error")
-        .defaultSuccessUrl(siteConfig.getBaseUrl() + "/")
+        .loginPage("/login")
+        .loginProcessingUrl("/login")
+        .failureUrl("/login?error")
+        .defaultSuccessUrl(siteConfig.getBaseUrl())
         .permitAll();
 
     http.rememberMe().key("remember-me").rememberMeServices(persistentTokenBasedRememberMeServices());
@@ -85,7 +90,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .logoutSuccessUrl(siteConfig.getBaseUrl() + "/")
         .deleteCookies("JSESSIONID", "remember-me");
 
-    http.addFilterBefore(yiiuFilterSecurityInterceptor, FilterSecurityInterceptor.class);
+    http.addFilterAfter(yiiuFilterSecurityInterceptor, FilterSecurityInterceptor.class);
 //    http.addFilterBefore(validateCodeAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     http.csrf().ignoringAntMatchers("/upload", "/user/space/deleteFile");
@@ -105,13 +110,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Bean
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
-  }
-
-  @Bean
-  public SavedRequestAwareAuthenticationSuccessHandler savedRequestAwareAuthenticationSuccessHandler() {
-    SavedRequestAwareAuthenticationSuccessHandler auth = new SavedRequestAwareAuthenticationSuccessHandler();
-    auth.setTargetUrlParameter("targetUrl");
-    return auth;
   }
 
   @Bean
