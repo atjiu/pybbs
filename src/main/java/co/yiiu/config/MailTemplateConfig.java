4f5ac02393a1430bbedc5c84add061a4d9048022
teddyzhu <teddyzhu15@gmail.com>
2017-11-11 15:46:02 +0800

    邮件主题,内容可配置

---
+++ src/main/java/co/yiiu/config/MailTemplateConfig.java
@@ -0,0 +1,27 @@
+package co.yiiu.config;
+
+/**
+ * Created by teddyzhu.
+ * Copyright (c) 2017, All Rights Reserved.
+ */
+public class MailTemplateConfig {
+
+    private String subject;
+    private String content;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
