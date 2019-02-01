8f23153b74645baca0288326e9172b93222367a6
tomoya92 <py2qiuse@gmail.com>
2019-02-01 14:49:35 +0800

    完善主题开发文档，超级详细，看了保证会开发主题

--- src/main/java/co/yiiu/pybbs/config/ServerRunner.java
+++ src/main/java/co/yiiu/pybbs/config/ServerRunner.java
@@ -1,6 +1,7 @@
 package co.yiiu.pybbs.config;
 
 import co.yiiu.pybbs.config.service.ElasticSearchService;
+import co.yiiu.pybbs.model.SystemConfig;
 import co.yiiu.pybbs.service.SystemConfigService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 /**
@@ -39,28 +43,28 @@ public class ServerRunner implements CommandLineRunner {
     }
 
     // 获取templates/theme里所有的主题，更新system_config表中的theme数据
-//    String activeEnv = null;
-//    String[] activeProfiles = environment.getActiveProfiles();
-//    if (activeProfiles.length > 0) {
-//      activeEnv = activeProfiles[0];
-//    }
-//    File file = null;
-//    if ("dev".equals(activeEnv) || "docker".equals(activeEnv)) {
-//      file = ResourceUtils.getFile("classpath:templates/theme");
-//    } else if ("prod".equals(activeEnv)) {
-//      file = ResourceUtils.getFile("file:./templates/theme");
-//    }
-//    if (file != null) {
-//      File[] files = file.listFiles();
-//      String[] themes = new String[files.length];
-//      for (int i = 0; i < files.length; i++) {
-//        if (files[i].isDirectory()) {
-//          themes[i] = files[i].getName();
-//        }
-//      }
-//      SystemConfig systemConfig = systemConfigService.selectByKey("theme");
-//      systemConfig.setOption(StringUtils.arrayToCommaDelimitedString(themes));
-//      systemConfigService.updateByKey("theme", systemConfig);
-//    }
+    String activeEnv = null;
+    String[] activeProfiles = environment.getActiveProfiles();
+    if (activeProfiles.length > 0) {
+      activeEnv = activeProfiles[0];
+    }
+    File file = null;
+    if ("dev".equals(activeEnv) || "docker".equals(activeEnv)) {
+      file = ResourceUtils.getFile("classpath:templates/theme");
+    } else if ("prod".equals(activeEnv)) {
+      file = ResourceUtils.getFile("file:./templates/theme");
+    }
+    if (file != null) {
+      File[] files = file.listFiles();
+      String[] themes = new String[files.length];
+      for (int i = 0; i < files.length; i++) {
+        if (files[i].isDirectory()) {
+          themes[i] = files[i].getName();
+        }
+      }
+      SystemConfig systemConfig = systemConfigService.selectByKey("theme");
+      systemConfig.setOption(StringUtils.arrayToCommaDelimitedString(themes));
+      systemConfigService.updateByKey("theme", systemConfig);
+    }
   }
 }
