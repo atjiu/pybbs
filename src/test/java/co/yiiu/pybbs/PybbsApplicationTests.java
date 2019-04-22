9c953af350e3f67a06d83204e0db7f28bc5de495
tomoya92 <py2qiuse@gmail.com>
2019-04-22 09:41:26 +0800

    更新文档

--- src/test/java/co/yiiu/pybbs/PybbsApplicationTests.java
+++ src/test/java/co/yiiu/pybbs/PybbsApplicationTests.java
@@ -1,14 +1,27 @@
 package co.yiiu.pybbs;
 
+import co.yiiu.pybbs.util.identicon.Identicon;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PybbsApplicationTests {
+
+  @Autowired
+  Identicon identicon;
 
   @Test
   public void contextLoads() {
 
+    identicon.generator("殷雷雷");
+    identicon.generator("maodou0217");
+    identicon.generator("abc");
+    identicon.generator("mywaya");
+    identicon.generator("tomoya");
   }
 
 }
