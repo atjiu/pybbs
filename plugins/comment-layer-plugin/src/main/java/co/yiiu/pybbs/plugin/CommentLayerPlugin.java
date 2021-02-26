52accbdc2c2ab3bd51a3a2161e8e292f22b31bca
tomoya92 <py2qiuse@gmail.com>
2021-02-26 15:46:59 +0800

    更新盖楼代码，修正楼层顺序

--- plugins/comment-layer-plugin/src/main/java/co/yiiu/pybbs/plugin/CommentLayerPlugin.java
+++ plugins/comment-layer-plugin/src/main/java/co/yiiu/pybbs/plugin/CommentLayerPlugin.java
@@ -18,55 +18,60 @@ import java.util.List;
 @Aspect
 public class CommentLayerPlugin {
 
-  @Around("co.yiiu.pybbs.hook.CommentServiceHook.selectByTopicId()")
-  public Object selectByTopicId(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-    List<CommentsByTopic> newComments =
-        (List<CommentsByTopic>) proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
-    // 盖楼显示评论
-    return this.sortByLayer(newComments);
-  }
-
-  // 从列表里查找指定值的下标
-  private int findLastIndex(List<CommentsByTopic> newComments, String key, Integer value) {
-    int index = -1;
-    for (int i = 0; i < newComments.size(); i++) {
-      if (key.equals("commentId")) {
-        if (value.equals(newComments.get(i).getCommentId())) {
-          index = i;
-        }
-      } else if (key.equals("id")) {
-        if (value.equals(newComments.get(i).getId())) {
-          index = i;
-        }
-      }
+    @Around("co.yiiu.pybbs.hook.CommentServiceHook.selectByTopicId()")
+    public Object selectByTopicId(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        List<CommentsByTopic> newComments =
+                (List<CommentsByTopic>) proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
+        // 盖楼显示评论
+        return this.sortByLayer(newComments);
     }
-    return index;
-  }
 
-  // 盖楼排序
-  private List<CommentsByTopic> sortByLayer(List<CommentsByTopic> comments) {
-    List<CommentsByTopic> newComments = new ArrayList<>();
-    comments.forEach(comment -> {
-      if (comment.getCommentId() == null) {
-        newComments.add(comment);
-      } else {
-        int index = this.findLastIndex(newComments, "commentId", comment.getCommentId());
-        if (index == -1) {
-          int upIndex = this.findLastIndex(newComments, "id", comment.getCommentId());
-          if (upIndex == -1) {
-            newComments.add(comment);
-          } else {
-            int layer = newComments.get(upIndex).getLayer() + 1;
-            comment.setLayer(layer);
-            newComments.add(upIndex + 1, comment);
-          }
-        } else {
-          int layer = newComments.get(index).getLayer();
-          comment.setLayer(layer);
-          newComments.add(index + 1, comment);
+    // 从列表里查找指定值的下标
+    private int findLastIndex(List<CommentsByTopic> newComments, String key, Integer value) {
+        int index = -1;
+        for (int i = 0; i < newComments.size(); i++) {
+            if (key.equals("commentId")) {
+                if (value.equals(newComments.get(i).getCommentId())) {
+                    index = i;
+                }
+            } else if (key.equals("id")) {
+                if (value.equals(newComments.get(i).getId())) {
+                    index = i;
+                }
+            }
         }
-      }
-    });
-    return newComments;
-  }
+        return index;
+    }
+
+    // 盖楼排序
+    // ！！！想来想去还是要用到两层for循环，求大神优化这部分代码，越快越好！！！
+    private List<CommentsByTopic> sortByLayer(List<CommentsByTopic> comments) {
+        List<CommentsByTopic> newComments = new ArrayList<>();
+        for (CommentsByTopic comment : comments) {
+            if (comment.getCommentId() == null) {
+                newComments.add(comment);
+            } else {
+                int idIndex = -1, commentIdIndex = -1;
+                boolean idIndexFlag = false, commentIdIndexFlag = false;
+                for (int i = newComments.size() - 1; i >= 0; i--) {
+                    if (!idIndexFlag && comment.getCommentId().equals(newComments.get(i).getId())) {
+                        idIndex = i;
+                        idIndexFlag = true;
+                    }
+                    if (!commentIdIndexFlag && comment.getCommentId().equals(newComments.get(i).getCommentId())) {
+                        commentIdIndex = i;
+                        commentIdIndexFlag = true;
+                    }
+                }
+                if (idIndex == -1) {
+                    newComments.add(comment);
+                } else {
+                    int layer = newComments.get(idIndex).getLayer();
+                    comment.setLayer(layer + 1);
+                    newComments.add(commentIdIndex == -1 ? idIndex + 1 : commentIdIndex + 1, comment);
+                }
+            }
+        }
+        return newComments;
+    }
 }
--- src/main/java/co/yiiu/pybbs/model/vo/CommentsByTopic.java
+++ src/main/java/co/yiiu/pybbs/model/vo/CommentsByTopic.java
@@ -3,6 +3,8 @@ package co.yiiu.pybbs.model.vo;
 import co.yiiu.pybbs.model.Comment;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Created by tomoya.
@@ -17,6 +19,8 @@ public class CommentsByTopic extends Comment implements Serializable {
     private String avatar;
     // 评论的层级，直接评论话题的，layer即为0，如果回复了评论的，则当前回复的layer为评论对象的layer+1
     private Integer layer;
+
+    private LinkedHashMap<Integer, List<CommentsByTopic>> children;
 
     public String getUsername() {
         return username;
@@ -40,5 +44,13 @@ public class CommentsByTopic extends Comment implements Serializable {
 
     public void setLayer(Integer layer) {
         this.layer = layer;
+    }
+
+    public LinkedHashMap<Integer, List<CommentsByTopic>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(LinkedHashMap<Integer, List<CommentsByTopic>> children) {
+        this.children = children;
     }
 }
