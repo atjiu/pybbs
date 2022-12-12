2bc31780b8798dfb8dd07967d310ee45f0376b2b
valavala <py2qiuse@gmail.com>
2022-12-12 14:01:59 +0800

    去掉sql中数据库名前缀

--- src/main/resources/db/migration/V1.16__add_column_content_style.sql
+++ src/main/resources/db/migration/V1.16__add_column_content_style.sql
@@ -12,5 +12,5 @@ UPDATE `comment`
 set `style` = "MD"
 where `style` is null;
 
-INSERT INTO `pybbs`.`system_config` (`key`, `value`, `description`, `pid`, `type`, `option`, `reboot`)
+INSERT INTO `system_config` (`key`, `value`, `description`, `pid`, `type`, `option`, `reboot`)
 VALUES ('content_style', 'MD', '发帖或者回复的输入框语法风格', '23', 'select', 'RICH,MD', '1');
